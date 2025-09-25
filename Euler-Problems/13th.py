71f2b4c8e3549ce43c037298ec15324919161189
Furkan Dicleli <147285295+furkandcll@users.noreply.github.com>
2025-09-26 01:22:41 +0300

    Delete Euler-Problems directory

--- Euler-Problems/13th.py
+++
@@ -1,7 +0,0 @@
-with open("13th.txt", "r", encoding="utf-8") as f:
-    numbers = [int(line.strip()) for line in f]
-
-total = sum(numbers)
-first_10 = str(total)[:10]
-
-print(first_10)
--- Euler-Problems/15th.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def path_count(n, m):
-    return math.comb(n + m, n)
-
-print(path_count(20, 20))
