8f8ef42ac44f17e05772fa8e113d8069a4f4b42e
Furkan Dicleli <147285295+furkandcll@users.noreply.github.com>
2025-10-01 00:31:24 +0300

    Update 21st.py

--- 21st.py
+++ 21st.py
@@ -1,1 +1,41 @@
+def amicable():
 
+    results = []
+
+    def divisor_finder(limit=10_000):
+        cache = {1: [1]}
+
+        def get_divisors(n):
+            if n in cache:
+                return cache[n]
+
+            divisors = []
+            for j in range(1, n // 2 + 1):
+                if n % j == 0:
+                    divisors.append(j)
+
+            cache[n] = divisors
+            return divisors
+
+        for i in range(1, limit):
+            divisors = get_divisors(i)
+            results.append({"Number": i, "divisors": divisors})
+        return results
+
+    amicables = []
+
+    x = divisor_finder()
+
+    for i in range(len(x)):
+
+        a = x[i]["Number"]
+        da = sum(x[i]["divisors"])
+
+        if da < len(x) and da != a:
+            db = sum(x[da - 1]["divisors"])
+            if db == a:
+                amicables.append({"digit1": a, "digit2": da})
+
+    return amicables
+
+print(len(amicable()) // 2)
